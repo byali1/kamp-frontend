a317001419bedbb0b93c4225df2482e1a3100277
byali <cmpunk_ilgin@hotmail.com>
2023-01-18 20:28:10 +0300

    Kamp_FrontEnd

--- northwind/src/app/app.component.ts
+++ northwind/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+
+//data burada yönetilir
+
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //beni app-root olarak kullan
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'northwind';
+  user: string = "Ali";
+  
 }
---
+++ northwind/src/app/models/product.ts
@@ -0,0 +1,10 @@
+
+//Product'ı karşılarız
+export interface Product{
+    productId:number;
+    categoryId:number;
+    productName:string;
+    unitPrice:number;
+    unitsInStock:number;
+    
+}
