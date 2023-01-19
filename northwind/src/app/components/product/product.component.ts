015aa7e2fc5c8dd381fcde99ade178db10755598
byali <cmpunk_ilgin@hotmail.com>
2023-01-19 13:45:51 +0300

    services added

--- northwind/src/app/components/product/product.component.ts
+++ northwind/src/app/components/product/product.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import {HttpClient} from '@angular/common/http'
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
- 
+export class ProductComponent implements OnInit {
+  products:Product[] = [];
+  dataLoaded = false;
+  
+  //constructor'ın amacı componenti bellekte new'lemek
+  constructor(private productService:ProductService){}
 
-  products:Product[] = [
-    
-  ];
+  //Component calistiginda calisan ilk method
+  ngOnInit():void{
+    this.getProducts();
+  }
 
-  constructor(){}
-
-  ngOnInit():void{}
+  getProducts(){
+    console.log("Api request started!")
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data;
+      this.dataLoaded = true; //şimdi senkron
+      console.log("Api request done!")
+     
+    })
+    console.log("Method done!")
+  }
 }
