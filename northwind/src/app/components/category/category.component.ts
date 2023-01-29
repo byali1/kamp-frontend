38fee97e8fde8513b3a67a8a6efa96b44e8bd5eb
byali <cmpunk_ilgin@hotmail.com>
2023-01-29 22:54:07 +0300

    Ability to select products by category

--- northwind/src/app/components/category/category.component.ts
+++ northwind/src/app/components/category/category.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
+  currentCategory: Category;
+  dataLoaded = false;
 
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+      this.dataLoaded = true; //şimdi senkron
+    });
+  }
+
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
+  }
+
+  getCurrentCategory(category: Category) {
+    if (category == this.currentCategory) {
+      return 'list-group-item list-group-item-action active';
+    }
+    return 'list-group-item list-group-item-action';
+  }
 }
